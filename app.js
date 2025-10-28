5d5f978852f3f2d70b85e8f629158d7eb127e171
Yevhenii Pozniak <85741218+Pozniak17@users.noreply.github.com>
2025-10-28 16:34:53 +0200

    Revert "update"

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import morgan from "morgan";
 import cors from "cors";
 
 import contactsRouter from "./routes/contactsRouter.js";
-import "./server.js";
 
 const app = express();
 
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,15 +1,15 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().required().min(3).max(15),
-  email: Joi.string().email().required(),
+  name: Joi.string().required(),
+  email: Joi.string().required(),
   phone: Joi.string().required(),
-  favorite: Joi.boolean().required(),
 });
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(15),
-  email: Joi.string().email(),
-  phone: Joi.string(),
-  favorite: Joi.boolean().required(),
-});
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string().max(7),
+})
+  .min(1)
+  .message("Body must have at least one field");
