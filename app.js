662df7a274adf22f572245bb30c7aaeafafbb05e
Yevhenii Pozniak <jekamanu@gmail.com>
2026-01-05 10:11:11 +0200

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import "dotenv/config";
 import contactsRouter from "./routes/contactsRouter.js";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(morgan("tiny"));
 app.use(cors());
